781ce0176c3f71a3810c7f98bb9a2631dcc57bd7
CollinHerber <rustaddicts@gmail.com>
2024-07-17 08:52:22 -0400

    Adding examples

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import './style.css';
+import '../@ncino/web-components' //This would be replaced with the npm package
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h3>Using Lit Components inside Vanilla Typescript</h3>
+    <nsc-button variant="brand">A SLDS Button</nsc-button>
+    <nmc-button>A Material Button</nmc-button>
   </div>
 `;
