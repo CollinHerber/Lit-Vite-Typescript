17964f4fd2ba056d32b697baf2c10d81c7e97f54
CollinHerber <rustaddicts@gmail.com>
2024-11-21 09:23:04 -0500

    Adding in a regular card example

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,21 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="token-area">
         <h3>Designing a NGC Button with SLDS using Tokens</h3>
         <ngc-button>A Gator Button With Tokens</ngc-button>
+
+        <ngc-card elevated="true">
+        <ngc-card-title>Card Title</ngc-card-title>
+        <ngc-card-subtitle>Card Subtitle</ngc-card-subtitle>
+        <ngc-card-content>
+            Here is some content that you can put into a card. <br>
+            This is a lot and can be anything you'd like
+        </ngc-card-content>
+        <ngc-card-footer>
+            <div style="text-align: center">
+                Card Footer
+            </div>
+        </ngc-card-footer>
+      </ngc-card> 
     </div>
+
   </div>
 `;
